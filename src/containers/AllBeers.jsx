4eb8625ca828ecc2d2b8b7accb85de7b8bd423f4
Darryl Pargeter <darrylpargeter@gmail.com>
2021-09-03 12:39:44 +0100

    Added in simple router

---
+++ src/containers/AllBeers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AllBeers = () => {
+  return (
+    <h1>All Beers</h1>
+  );
+};
+
+export default AllBeers;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import useBeerData from '@hooks/useBeerData';
+import { Router, Link } from '@reach/router';
+import Shop from '@containers/Shop';
+import AllBeers from '@containers/AllBeers';
 
 const App = () => {
   const beers = useBeerData();
@@ -11,6 +14,16 @@ const App = () => {
   return (
     <div>
       <h2> Temp Header</h2>
+      <nav>
+        <Link to="/">Shop</Link>{" "}
+        <Link to="/all-beers">All Beers</Link>{" "}
+      </nav>
+
+      <Router>
+        <Shop path="/" />
+        <AllBeers path="all-beers" />
+      </Router>
+
       <button onClick={handleNextPageClick}>Next Page</button>
       <button onClick={handlePrevPageClick}>Prev Page</button>
     </div>
